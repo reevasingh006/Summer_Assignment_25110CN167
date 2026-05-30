3f79d33dd6973d9ea2cab5635efc9fd2cb09b8bf
reevasingh <reevasingh@REEVAs-MacBook-Air.local>
2026-05-30 18:27:26 +0530

    Added Day 3 assignment

---
+++ day3/day3_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, flag=1;
+    printf("enter a number :");
+    scanf("%d",&n);
+    if(n<=1)
+    flag=0;
+    else{
+        for(i=2;i<=n/2; i++){
+        if(n%i==0){
+            flag=0;
+            break;
+        }
+        }
+    }
+    if(flag)
+    printf("prime number");
+    else
+    printf("not a prime number");
+    return 0;
+
+}
---
+++ day3/day3_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int start, end, i, j,flag;
+    printf("enter start and end :");
+    scanf("%d %d",&start, &end);
+    for(i=start;i<=end; i++){
+        if(i<=1)
+        continue;
+        flag=1;
+        for(j=2; j<=i/2; j++){
+            if (i%j==0){
+                flag=0;
+                break;
+            }
+        }
+        if(flag)
+        printf("%d ",i);
+    }
+    return 0;
+}
